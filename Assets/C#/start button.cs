0942c28d26883e86c9f3790c044d77cf6d7cdfea
Fhunii <huni.fanta.4288@gmail.com>
2024-09-08 14:58:05 +0900

    Refactor GUID.txt and shadercompiler-AssetImportWorker0.log: Update WebGL build GUID

--- Assets/C#/start button.cs
+++ Assets/C#/start button.cs
@@ -32,14 +32,17 @@ public class ButtonScript : MonoBehaviour
         // Vector3 worldPos1 = myTransform1.position;
         // ボタンの位置を移動
 
-        for (int i = 0; i < 100; i++)
+        for (int i = 0; i < 80; i++)
         {
 
-            startTransform.position += new Vector3(10, 0, 0);
-            archiveTransform.transform.position += new Vector3(-10, 0, 0);
-            settingsTransform.transform.position += new Vector3(0, -5, 0);
+            startTransform.position += new Vector3(30, 0, 0);
+            archiveTransform.transform.position += new Vector3(-30, 0, 0);
+            settingsTransform.transform.position += new Vector3(0, -10, 0);
             if (i <= 30)
                 rogoTransform.transform.position += new Vector3(0, -0.08f, 0);
+
+            if (i > 30 && i <= 75)
+                rogoTransform.transform.localScale += new Vector3(2.5f, 2.5f, 2.5f);
 
             yield return new WaitForSeconds(0.01f);
 
